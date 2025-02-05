414e37debd2d3f3d58ae900547370e69df0ab33d
leventenyiri <nyiri.levi88@gmail.com>
2025-02-05 08:46:26 +0100

    debug

--- docs/conf_common.py
+++ docs/conf_common.py
@@ -6,25 +6,25 @@ extensions += [
     'esp_docs.esp_extensions.run_doxygen',
 ]
 
-# Repository and project settings
-github_user = 'leventenyiri'
-github_repo = 'ESP_Sentinel_Cam'
 
-# HTML context for theme and GitHub integration
-html_context['github_user'] = github_user
+# Project information
+project = 'ESP_Sentinel_Cam'
+author = 'Levente Nyiri'
+copyright = '2024, ' + author
+
+# Repository settings for ESP-Docs
+github_repo = 'ESP_Sentinel_Cam'
+html_context['github_user'] = 'leventenyiri'
 html_context['github_repo'] = github_repo
 html_context['github_version'] = 'main'
 
-# Project information
-project = github_repo
-author = 'Levente Nyiri, Bence Nyitrai'
-copyright = '2024, ' + author
+# Base URL settings
+html_baseurl = f'https://leventenyiri.github.io/{github_repo}/'
 
 # Documentation settings
 languages = ['en']
 language = 'en'
 todo_include_todos = True
-html_static_path = []
 
-# Theme settings
-html_baseurl = f'https://leventenyiri.github.io/{github_repo}/'
+# Override problematic settings from esp_docs.conf_docs
+html_static_path = []  # Remove the _static path since it doesn't exist
