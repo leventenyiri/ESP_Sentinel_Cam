7c80736e535990e9f69c394eaacdde29d4c7f4c2
leventenyiri <nyiri.levi88@gmail.com>
2025-02-05 08:41:54 +0100

    debug

--- docs/conf_common.py
+++ docs/conf_common.py
@@ -2,32 +2,29 @@ from esp_docs.conf_docs import *  # noqa: F403,F401
 
 extensions += [
     'sphinx_copybutton',
-    # Needed as a trigger for running doxygen
     'esp_docs.esp_extensions.dummy_build_system',
     'esp_docs.esp_extensions.run_doxygen',
 ]
 
-# Base URLs and repository settings
-github_repo = 'ESP_Sentinel_Cam'
+# Repository and project settings
 github_user = 'leventenyiri'
-github_url = f'https://github.com/{github_user}/{github_repo}'
-BASE_URL = github_url + '/blob/main'
-
-# Project information
-project = 'ESP_Sentinel_Cam'
-author = 'Levente Nyiri'
-copyright = '2024, ' + author
-
-# Repository settings for ESP-Docs
 github_repo = 'ESP_Sentinel_Cam'
-html_context['github_user'] = 'leventenyiri'
+
+# HTML context for theme and GitHub integration
+html_context['github_user'] = github_user
 html_context['github_repo'] = github_repo
 html_context['github_version'] = 'main'
 
-# Base URL settings
-html_baseurl = f'https://leventenyiri.github.io/{github_repo}/'
+# Project information
+project = github_repo
+author = 'Levente Nyiri, Bence Nyitrai'
+copyright = '2024, ' + author
 
 # Documentation settings
 languages = ['en']
 language = 'en'
 todo_include_todos = True
+html_static_path = []
+
+# Theme settings
+html_baseurl = f'https://leventenyiri.github.io/{github_repo}/'
--- docs/en/index.rst
+++ docs/en/index.rst
@@ -1,7 +1,7 @@
 ESP_SENTINEL_CAM
 ================
 
-**Documentation for the AITIA starling project.**
+**Documentation for the AITIA starling project.** Helo
 
 .. toctree::
     :hidden:
